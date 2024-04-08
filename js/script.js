1be8bb45ee4d695e0aaf73e2225e39db8b1a74ce
A K M Ziaul Hasan <159991542+Ziaul6525@users.noreply.github.com>
2024-04-08 13:51:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction2(){
+    document.getElementById("demo2").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction3(){
+    document.getElementById("demo3").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction4(){
+    document.getElementById("demo4").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction5(){
+    document.getElementById("demo5").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction6(){
+    document.getElementById("demo6").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction7(){
+    document.getElementById("demo7").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction8(){
+    document.getElementById("demo8").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,";
+}
+function myFunction114(){
+    document.getElementById("demo114").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ad dolor ab pariatur nemo vel sit, totam libero possimus, reprehenderit temporibus qui atque sint cumque aliquid cum quidem voluptate incidunt consectetur perferendis perspiciatis eligendi,
+}
+
+  
